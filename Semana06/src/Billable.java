dcd512dfe47a0929deec3c0007f26bd8af1a8af4
Johanna ortiz <johanita12ortiz@gmail.com>
2025-12-05 12:43:47 -0500

    Delete Semana06 directory

--- Semana06/src/Billable.java
+++
@@ -1,9 +0,0 @@
-// Archivo: src/Semana06/Billable.java
-
-// COMENTARIO: Esta es una INTERFAZ. Define la CAPACIDAD de un objeto para ser facturado.
-// Las clases que la implementan son responsables de definir la lógica de costo.
-public interface Billable {
-    double getBaseCost();
-    double calculateTaxes();
-    double calculateFinalCost();
-}
